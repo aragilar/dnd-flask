5ed757b8777b431aba09a5e8cdd28222cdd8df6b
Omega0 <cphantom4.0@gmail.com>
2016-07-17 22:24:32 -0500

    Minor fix to collapse all button

--- static/accordion.js
+++ static/accordion.js
@@ -6,9 +6,21 @@ $(document).ready(function(){
         var elem = $(this);
         var next = elem.next();
         if (next.is('dd')){
-            next.slideToggle(400);
-            elem.toggleClass('dt-closed dt-open');
-            next.toggleClass('dd-closed dd-open');
+            if (next.hasClass('dd-closed')){
+                next.slideToggle(400);
+                elem.removeClass('dt-closed').addClass('dt-open');
+                next.removeClass('dd-closed').addClass('dd-open');
+            } else {
+                next.slideUp(400);
+                elem.removeClass('dt-open').addClass('dt-closed');
+                next.removeClass('dd-open').addClass('dd-closed');
+            }
         }
     });
 });
+
+function closeAccordions(){
+    $('.accordion > dd').removeClass('dd-open').addClass('dd-closed').slideUp();
+    $('.accordion > dt').removeClass('dt-open').addClass('dt-closed');
+    $('._preserve').removeClass('_preserve');
+}
