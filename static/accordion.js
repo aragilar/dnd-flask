f604f566b5046a60d1d0db1bbf482cb2fb7191fb
Omega0 <cphantom4.0@gmail.com>
2016-09-14 17:22:46 -0500

    Style changes
    Mostly the accordion transition

--- static/accordion.js
+++ static/accordion.js
@@ -3,15 +3,16 @@ $(document).ready(function(){
     $('.accordion > dd').addClass('dd-closed').hide();
     
     $('.accordion > dt').click(function(){
+        var transition = 250;//ms
         var elem = $(this);
         var next = elem.next();
         if (next.is('dd')){
             if (next.hasClass('dd-closed')){
-                next.slideDown(400);
+                next.slideDown(transition);
                 elem.removeClass('dt-closed').addClass('dt-open');
                 next.removeClass('dd-closed').addClass('dd-open');
             } else {
-                next.slideUp(400);
+                next.slideUp(transition);
                 elem.removeClass('dt-open').addClass('dt-closed');
                 next.removeClass('dd-open').addClass('dd-closed');
             }
